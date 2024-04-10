2bf8a05bdaf255fa57226898488accd875f791e4
SkimoCHK <alexguillen265@gmail.com>
2024-04-09 17:49:28 -0700

    Agregar archivos de proyecto.

---
+++ AlgoNuevo/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace AlgoNuevo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 3;
+            int suma;
+            int producto;
+
+            CalcularSumaYProducto(a, b, out suma, out producto);
+
+            Console.WriteLine("La suma de {0} y {1} es: {2}", a, b, suma);
+            Console.WriteLine("El producto de {0} y {1} es: {2}", a, b, producto);
+        }
+
+        static void CalcularSumaYProducto(int x, int y, out int sum, out int product)
+        {
+            sum = x + y;
+            product = x * y;
+        }
+
+    }
+}
